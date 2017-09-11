b443da5751fcb97511ba2ac1e86facf61bb2c834
ciara hudson <cchudson33@gmail.com>
2017-09-11 17:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var height = "74"
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Susan")   
-if (height === "74")
-    console.log("The height is correct") {
-      
-    }
- }
+    console.log("Expected the name to be Susan")
+}
+if (height === "74"){
+    console.log("The height is correct")
+  }
 
 // Don't worry about this
 module.exports = { name, height
 }
-
